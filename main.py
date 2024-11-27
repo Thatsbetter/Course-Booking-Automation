bb53fb1bf3f7c2bd2c840d9c1f7e066de137126b
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2024-11-27 23:49:20 +0100

    Add scheduler

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
-
+import schedule  # Import the schedule library
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
@@ -8,9 +9,77 @@ from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from credential import Credential
 
+def job(course_id):
+    # Initialize the browser and perform all actions here
+    with initialize_browser() as browser:
+        browser.get("https://buchung.hochschulsport-hamburg.de/angebote/Wintersemester_2024_2025/_Volleyball.html")
+        time.sleep(1)
+        browser.find_element(By.NAME, course_id).click()
+        try:
+            cred = Credential()
+            original_window = browser.current_window_handle
+            window_handles = browser.window_handles
+
+            for handle in window_handles:
+                if handle != original_window:
+                    browser.switch_to.window(handle)
+                    break
+
+            first_submit_button = browser.find_element(By.XPATH, '//input[@type="submit"]')
+            browser.execute_script("arguments[0].scrollIntoView(true);", first_submit_button)
+            first_submit_button.click()
+            time.sleep(1)
+
+            parent_element = browser.find_element(By.ID, "bs_pw_anm")
+            browser.execute_script("arguments[0].classList.remove('hidden');", parent_element)
+
+            element = browser.find_element(By.CSS_SELECTOR, ".bs_form_infotext")
+            browser.execute_script("arguments[0].click();", element)
+
+            parent_element = WebDriverWait(browser, 10).until(
+                EC.presence_of_element_located((By.ID, "bs_pw_anm"))
+            )
+            browser.execute_script("arguments[0].classList.remove('hidden');", parent_element)
+
+            email_field = WebDriverWait(browser, 10).until(
+                EC.presence_of_element_located((By.XPATH, "/html/body/form/div/div[2]/div[1]/div[2]/div[2]/input"))
+            )
+
+            email_field.send_keys(cred.get_username())
+
+            password_field = WebDriverWait(browser, 10).until(
+                EC.presence_of_element_located((By.XPATH, "/html/body/form/div/div[2]/div[1]/div[3]/div[2]/input"))
+            )
+            password_field.send_keys(cred.get_password())
+
+            confirm_login = WebDriverWait(browser, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR,
+                                                "div.bs_form_foot:nth-child(5) > div:nth-child(1) > div:nth-child(2) > input:nth-child(1)"))
+            )
+            confirm_login.click()
+
+            confirm_AGB = WebDriverWait(browser, 10).until(
+                EC.presence_of_element_located((By.XPATH, "/html/body/form/div/div[3]/div[2]/label/input")))
+            confirm_AGB.click()
+            time.sleep(1)
+            submit_form = WebDriverWait(browser, 10).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@id="bs_submit"]'))
+            )
+            submit_form.click()
+            time.sleep(1)
+            final_submit = WebDriverWait(browser, 10).until(
+                EC.presence_of_element_located((By.XPATH, '/html/body/form/div/div[3]/div[1]/div[2]'))
+            )
+            final_submit.click()
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+        time.sleep(2)
+
 def initialize_browser():
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless=new")
+    options.add_argument("--headless=new")
     options.add_argument("--window-size=1280,700")
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
@@ -20,81 +89,11 @@ def initialize_browser():
     browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
     return browser
 
+# Schedule the job for Thursdays at 21:01 local time
+schedule.every().monday.at("19:31").do(lambda:job("BS_Kursid_78053"))
+schedule.every().thursday.at("21:01").do(lambda:job("BS_Kursid_79594"))
 
-with initialize_browser() as browser:
-    browser.get("https://buchung.hochschulsport-hamburg.de/angebote/Wintersemester_2024_2025/_Volleyball.html")
+# Keep the script running
+while True:
+    schedule.run_pending()
     time.sleep(1)
-    browser.find_element(By.NAME, 'BS_Kursid_78052').click()
-    try:
-        cred = Credential()
-        # Get the current window handles
-        original_window = browser.current_window_handle
-        window_handles = browser.window_handles
-
-        # Switch to the new tab
-        for handle in window_handles:
-            if handle != original_window:
-                browser.switch_to.window(handle)
-                break
-        # Perform actions in the new tab
-        first_submit_button = browser.find_element(By.XPATH, '//input[@type="submit"]')
-        browser.execute_script("arguments[0].scrollIntoView(true);", first_submit_button)
-        first_submit_button.click()
-        time.sleep(1)
-        # Locate the element
-        ###########################
-
-        parent_element = browser.find_element(By.ID, "bs_pw_anm")
-        browser.execute_script("arguments[0].classList.remove('hidden');", parent_element)
-
-        # Locate the target element after making it visible
-        element = browser.find_element(By.CSS_SELECTOR, ".bs_form_infotext")
-
-        # Execute JavaScript to click on the element
-        browser.execute_script("arguments[0].click();", element)
-
-        ###########################
-
-        # Wait for the parent element to be present and then remove the 'hidden' class
-        parent_element = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.ID, "bs_pw_anm"))
-        )
-        browser.execute_script("arguments[0].classList.remove('hidden');", parent_element)
-
-        # Input "username" into the email field using its custom attribute
-        email_field = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.XPATH, "/html/body/form/div/div[2]/div[1]/div[2]/div[2]/input"))
-        )
-        email_field.send_keys(cred.get_username())
-
-        # Input "user" into the password field using its custom attribute
-        password_field = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.XPATH, "/html/body/form/div/div[2]/div[1]/div[3]/div[2]/input"))
-        )
-        password_field.send_keys(cred.get_password())
-
-        confirm_login = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR,
-                                            "div.bs_form_foot:nth-child(5) > div:nth-child(1) > div:nth-child(2) > input:nth-child(1)"))
-        )
-        confirm_login.click()
-
-        ##################
-        confirm_AGB = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.XPATH, "/html/body/form/div/div[3]/div[2]/label/input")))
-        confirm_AGB.click()
-        time.sleep(1)
-        submit_form = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="bs_submit"]'))
-        )
-        submit_form.click()
-        time.sleep(1)
-        final_submit = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.XPATH, '/html/body/form/div/div[3]/div[1]/div[2]'))
-        )
-        final_submit.click()
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-    time.sleep(2)  # Optional: Wait for 5 seconds before closing the browser
