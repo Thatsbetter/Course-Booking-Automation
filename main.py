d16aa15b2f839247a04780c0ce3322903f8095d8
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2024-11-29 13:18:52 +0100

    Add time converter from utc+1 to server time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
 import schedule  # Import the schedule library
-from datetime import datetime
+from datetime import datetime,timedelta
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
@@ -8,6 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from credential import Credential
+import pytz
 
 def job(course_id):
     # Initialize the browser and perform all actions here
@@ -71,7 +72,6 @@ def job(course_id):
                 EC.presence_of_element_located((By.XPATH, '/html/body/form/div/div[3]/div[1]/div[2]'))
             )
             final_submit.click()
-
         except Exception as e:
             print(f"An error occurred: {e}")
 
@@ -89,9 +89,29 @@ def initialize_browser():
     browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
     return browser
 
+def determine_server_time_offset():
+    """Determine the server's offset from UTC"""
+    utc_now = datetime.utcnow()
+    local_now = datetime.now()
+    offset_seconds = (local_now - utc_now).total_seconds()
+
+    return offset_seconds
+
+
+def convert_to_server_time(berlin_hour, berlin_minute):
+    """Convert given Berlin time to server's local time"""
+    berlin_timezone = pytz.timezone('Europe/Berlin')
+    berlin_time = datetime.now(berlin_timezone).replace(hour=berlin_hour, minute=berlin_minute, second=0, microsecond=0)
+    utc_time = berlin_time.astimezone(pytz.utc)
+    server_offset_seconds = determine_server_time_offset()
+    server_time = utc_time + timedelta(seconds=server_offset_seconds)
+    return server_time.time()
+
+
+
 # Schedule the job for Thursdays at 21:01 local time
-schedule.every().monday.at("19:31").do(lambda:job("BS_Kursid_78053"))
-schedule.every().thursday.at("21:01").do(lambda:job("BS_Kursid_79594"))
+schedule.every().monday.at(convert_to_server_time(19, 31).strftime("%H:%M")).do(lambda:job("BS_Kursid_78053"))
+schedule.every().thursday.at(convert_to_server_time(21,1).strftime("%H:%M")).do(lambda:job("BS_Kursid_79594"))
 
 # Keep the script running
 while True:
